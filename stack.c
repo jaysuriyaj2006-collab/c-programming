56c8709c7718e93e2cc03e8b1e5b3c828d68f9ce
jaysuriyaj2006-collab <jaysuriyaj2006@gmail.com>
2026-02-04 09:32:50 +0530

    added files

--- stack.c
+++ stack.c
@@ -12,12 +12,41 @@ void push(int value){
         printf("%d value pushed to array\n",value);
     }
 }
+void pop(int value){
+    if(top==size-1){
+        printf("no space left\n");
+    }
+    else{
+        top--;
+        array[top]=value;
+        printf("%d value can be poped\n",value);
+    }
+}
+void peak(){
+    if(top==-1){
+        printf("No value at stack");
+    }
+    else{
+        printf("The peak value is :%d\n",array[top]);
+    }
+}
+void isempty(){
+    if(top==-1){
+        printf("No value at stack\n");
+    }
+    else{
+        printf("stack has items\n");
+    }
+}
+void Size(){
+    printf("my stack size is %zu \n",top+1);
+}
+
 int main(){
     push(10);
     push(20);
-    push(30);
-    push(40);
-    push(50);
-    push(60);
+    pop(20);
+    isempty();
+    Size();
     return 0;
 }
